039d97fa9e1ce14015bd74f5f4eab0e747e0e353
Junior S <juniorsuarez1014@gmail.com>
2020-11-14 17:25:14 -0500

    adding files to repositorty 2

---
+++ GHFollowersSecond/GFBodyLabel.swift
@@ -0,0 +1,21 @@
+//
+//  GFBodyLabel.swift
+//  GHFollowersSecond
+//
+//  Created by Junior Suarez-Leyva on 9/22/20.
+//  Copyright © 2020 Junior Suarez-Leyva. All rights reserved.
+//
+
+import UIKit
+
+class GFBodyLabel: UILabel {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ GHFollowersSecond/GFTitleLabel.swift
@@ -0,0 +1,13 @@
+//
+//  GFTitleLabel.swift
+//  GHFollowersSecond
+//
+//  Created by Junior Suarez-Leyva on 9/22/20.
+//  Copyright © 2020 Junior Suarez-Leyva. All rights reserved.
+//
+
+import UIKit
+
+class GFTitleLabel: UILabel {
+
+}
